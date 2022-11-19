9051fce16f8bd670bd53dddbebbb356590ca6a90
ryanparketh <ryanpark@rocketmail.com>
2022-11-19 07:34:40 -0500

    added connection.js, env

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: process.env.DB_USER,
+    // TODO: Add MySQL password here
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  },
+  console.log(`Connected to the employee_db database.`)
+);
